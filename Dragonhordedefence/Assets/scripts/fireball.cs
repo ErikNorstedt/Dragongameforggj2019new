79bc5ed7202547df51782c189856f9bf3ee869c6
ErikNorstedt <47041639+ErikNorstedt@users.noreply.github.com>
2019-01-26 15:34:51 +0100

    Dragon is in

---
+++ Dragonhordedefence/Assets/scripts/fireball.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fireball : MonoBehaviour
+{
+
+    public float speed = 20f;
+    Rigidbody2D rb;
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.up * speed;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Dragonhordedefence/Assets/scripts/playerAim.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerAim : MonoBehaviour {
+
+    public Rigidbody2D rb;
+    public float speed = 5f;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, Input.GetAxisRaw("Vertical") * speed);
+
+        //Camera and rotating stuff
+        Vector3 mousepos = Input.mousePosition;
+        mousepos = Camera.main.ScreenToWorldPoint(mousepos);
+
+        Vector2 direction = new Vector2(
+            mousepos.x - transform.position.x,
+            mousepos.y - transform.position.y
+            );
+
+        transform.right = direction;
+
+    }
+}
