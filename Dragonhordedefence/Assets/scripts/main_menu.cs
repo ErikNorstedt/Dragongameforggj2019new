0216af92afb302d9d663d11aa4eb3f57b750b713
Fourzifer <30759328+Fourzifer@users.noreply.github.com>
2019-01-27 10:56:16 +0100

    Added menu

---
+++ Dragonhordedefence/Assets/scripts/main_menu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class main_menu : MonoBehaviour {
+
+    //change index to +1 in final version
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+    
+    public void QuitGame ()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+
+    //change index to -1 in final version
+    public void Retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex -2);
+    }
+}
